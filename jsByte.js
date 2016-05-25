40c22086fb1c0d3478089d3042c183f73ab4a45b
zaphod-42 <trey@tomadoh.com>
2016-05-25 13:51:31 -0500

    Added array to byte casting method

--- jsByte.js
+++ jsByte.js
@@ -73,3 +73,7 @@ Byte.prototype.valueOf = function(){
 Number.prototype.toByte = String.prototype.toByte = function(base, size){
     return new Byte(this, base, size);
 }
+Array.prototype.toByte = function(size){
+    return new Byte(this.join(""), 2, size);
+}
+}
